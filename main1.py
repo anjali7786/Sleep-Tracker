9d8c7fee985410b1bf71d41903c9daee63461e61
Hritika <hritika.cse29@gmail.com>
2021-07-28 19:44:09 +0530

    added img url in boostyourself, changed classes in login.css and added login and signup in python

--- main1.py
+++ main1.py
@@ -10,6 +10,14 @@ def home():
 def track_your_sleep():
     return render_template('Trackmysleep.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
 @app.route("/unabletosleep")
 def unable_to_sleep():
     return render_template('unabletosleep.html')
